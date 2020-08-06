a3ef39fc63c0dabf8faf0691c52f89d26d25d483
Akorwash <mr.ahmedkorwash@gmail.com>
2020-08-06 13:10:11 +0200

    start working with rest

--- main.go
+++ main.go
@@ -6,10 +6,26 @@ import (
 	"QuizBattle/handler"
 	"QuizBattle/service/createaccountservice"
 	"QuizBattle/service/loginservice"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	a.Run(":8010")
 	//Loading the game
 	engine.StartTheGame()
 
@@ -82,4 +98,70 @@ func main() {
 		}
 	}
 
+}
+
+//App web
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+//Initialize start project
+func (a *App) Initialize(user, password, dbname string) {
+	//connectionString :=
+	//fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
+	//var err error
+	//a.DB, err = sql.Open("postgres", connectionString)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	a.Router = mux.NewRouter()
+
+	a.initializeRoutes()
+}
+
+//Run the project
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8010", a.Router))
+}
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	/*p := product{ID: id}
+	    if err := p.getProduct(a.DB); err != nil {
+	        switch err {
+	        case sql.ErrNoRows:
+	            respondWithError(w, http.StatusNotFound, "Product not found")
+	        default:
+	            respondWithError(w, http.StatusInternalServerError, err.Error())
+	        }
+	        return
+		}*/
+
+	fmt.Println(id)
+	respondWithJSON(w, http.StatusOK, id)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/product/{id:[0-9]+}", a.getProduct).Methods("GET")
 }
