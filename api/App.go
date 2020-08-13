660d23a40ef377188e0e8d6eb3f9a42823412623
Akorwash <mr.ahmedkorwash@gmail.com>
2020-08-13 14:00:25 +0200

    refactor api handler

--- api/App.go
+++ api/App.go
@@ -42,8 +42,15 @@ func (a *App) Run(port string) {
 
 //initializeRoutes here we will intialize the rest apis routes and html pages
 func (a *App) initializeRoutes() {
-	a.Router.HandleFunc("/question/{id:[0-9]+}", questionController.GetQuestionByID(service.NewQuestionServices(repository.NewMongoQuestionRepository()))).Methods("GET")
-	a.Router.HandleFunc("/user/createuser", userController.CreateUser(createaccount.NEW(repository.NewMongoUserRepository()))).Methods("POST")
-	a.Router.HandleFunc("/user/login", userController.Login(login.New(repository.NewMongoUserRepository()))).Methods("POST")
+	questionRepo := repository.NewMongoQuestionRepository()
+	userRepo := repository.NewMongoUserRepository()
+
+	questionSvc := service.NewQuestionServices(questionRepo)
+	createAccSvc := createaccount.NEW(userRepo)
+	loginSvc := login.New(userRepo)
+
+	a.Router.HandleFunc("/question/{id:[0-9]+}", questionController.GetQuestionByID(questionSvc)).Methods("GET")
+	a.Router.HandleFunc("/user/createuser", userController.CreateUser(createAccSvc)).Methods("POST")
+	a.Router.HandleFunc("/user/login", userController.Login(loginSvc)).Methods("POST")
 	a.Router.HandleFunc("/", homeController.HomePage).Methods("GET")
 }
