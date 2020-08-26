eaef4cc348bf5aefee78f53d428d9f00489eeb4a
Akorwash <mr.ahmedkorwash@gmail.com>
2020-08-26 17:20:43 +0200

    success to send blob voice

--- src/websockets/client.go
+++ src/websockets/client.go
@@ -205,6 +205,7 @@ func ServeGameBattle(userID uint64, fullname string, w http.ResponseWriter, r *h
 
 var streamGamehub *Hub
 var streamworldchathub *Hub
+var streamvoicechathub *Hub
 
 //ServeGameStream handles websocket requests from the peer
 func ServeGameStream(userID uint64, fullname string, w http.ResponseWriter, r *http.Request) {
@@ -220,6 +221,28 @@ func ServeGameStream(userID uint64, fullname string, w http.ResponseWriter, r *h
 	}
 
 	client := &Client{UserID: userID, Fullname: fullname, hub: streamGamehub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
+}
+
+//ServeVoiceChatStream handles websocket requests from the peer
+func ServeVoiceChatStream(userID uint64, fullname string, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if streamvoicechathub == nil {
+		streamvoicechathub = NewHub()
+		streamvoicechathub.Run()
+	}
+
+	client := &Client{UserID: userID, Fullname: fullname, hub: streamvoicechathub, conn: conn, send: make(chan []byte, 4096)}
 	client.hub.register <- client
 
 	// Allow collection of memory referenced by the caller by doing all work in
