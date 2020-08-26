9a67b63e2d5272d3f0b7369536c3a10bcf59643e
احمد قرواش <mr.ahmedkorwash@gmail.com>
2020-08-27 00:12:59 +0200

    add docker file

--- src/datastore/entites/entities.go
+++ src/datastore/entites/entities.go
@@ -57,7 +57,6 @@ func comparePasswords(hashedPwd string, plainPwd []byte) bool {
 	byteHash := []byte(hashedPwd)
 	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
 	if err != nil {
-		log.Println(err)
 		return false
 	}
 
