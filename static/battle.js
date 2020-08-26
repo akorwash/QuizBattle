3dd664f246477eaf1730b54c7bb59cc7cb2dd362
Akorwash <mr.ahmedkorwash@gmail.com>
2020-08-26 13:06:22 +0200

    start build battel logic

--- static/battle.js
+++ static/battle.js
@@ -2,6 +2,11 @@
 var gameinformation = null;
 
 function loadgameinfo() {
+    
+    var battlepage = document.getElementById('battlepage')
+    $(battlepage).addClass('loader')
+    $(battlepage).css('display','block')
+
     var gameId = window.location.href.split('/')[4]
     $.ajax({
         type: 'get',
@@ -19,6 +24,8 @@ function loadgameinfo() {
         var data = JSON.parse(failObj.responseText);
         var errSpan = document.getElementById('errorSumm')
         errSpan.innerText = data.error
+        $(battlepage).removeClass('loader')
+        $(battlepage).css('display','none')
       });
 }
 
@@ -29,6 +36,18 @@ function titleedit() {
 
         var battleTitle = document.getElementById('battleTitle')
         battleTitle.innerText =  gameinformation.User.Fullname +" Battle"
+
+
+        gameinformation.JoinedUser.forEach(function(user){
+            var joined_users_list = document.getElementById("battle_Joined_Users_container");
+            var user_fiv = `<div class="bold">
+            <a href="" style="color:black; text-decoration:none;" ><span>`+user.Fullname+`</span></a><span style="color:green" class="p-3">Online</span>
+          </div>`
+
+            $(joined_users_list).append(user_fiv)
+        })
+        $(battlepage).removeClass('loader')
+        $(battlepage).css('display','none')
     }
 }
 
