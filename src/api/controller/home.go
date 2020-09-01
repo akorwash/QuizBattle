aa19b356b992fa1677d4736b68cb0bd982152f44
احمد قرواش <mr.ahmedkorwash@gmail.com>
2020-09-01 21:38:41 +0200

    add contact us page

--- src/api/controller/home.go
+++ src/api/controller/home.go
@@ -19,6 +19,16 @@ func (controller *HomeController) HomePage(w http.ResponseWriter, r *http.Reques
 	http.ServeFile(w, r, "./api/view/index.html")
 }
 
+//ContactUS recieve ContactUS form
+func (controller *HomeController) ContactUS(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(w, r, "./api/view/index.html")
+}
+
 //AboutPage about page http requst handler
 func (controller *HomeController) AboutPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/about" {
