ac275e17103bfd2d1399aae46309289559f3da6f
احمد قرواش <mr.ahmedkorwash@gmail.com>
2020-09-03 06:39:07 +0200

    development and production ws file js

--- src/static/urlbase.js
+++ src/static/urlbase.js
@@ -1,4 +1,11 @@
-const VOICE_WS = "wss://" + document.location.host + "/ws/voice/" + window.localStorage.getItem('auth_token')
-const PROFILE_WS = "wss://" + document.location.host + "/ws/" + window.localStorage.getItem('auth_token')
-const GAME_WS = "wss://" + document.location.host + "/ws/" + window.localStorage.getItem('auth_token')
-const WORLD_WS = "wss://" + document.location.host + "/ws/worldchat/" + window.localStorage.getItem('auth_token')
+var host = window.location.hostname;
+var pref_ws = "wss://"
+
+if(host == 'localhost') {
+    pref_ws = "ws://"
+}
+
+const VOICE_WS = pref_ws + document.location.host + "/ws/voice/" + window.localStorage.getItem('auth_token')
+const PROFILE_WS = pref_ws + document.location.host + "/ws/" + window.localStorage.getItem('auth_token')
+const GAME_WS = pref_ws + document.location.host + "/ws/" + window.localStorage.getItem('auth_token')
+const WORLD_WS = pref_ws + document.location.host + "/ws/worldchat/" + window.localStorage.getItem('auth_token')
